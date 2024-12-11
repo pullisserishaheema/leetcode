0d528850928cb557582fdac76b96d9b8fb01eba3
shaheema pullisseri <pullisserishaheema@gmail.com>
2024-12-11 23:41:26 +0530

    Create Hello World Function

---
+++ Create Hello World Function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    };
+};
+console.log()
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
